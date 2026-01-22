b8fa451b54cb453a53fb8c19d67ac37e3655f8aa
rometricsapp-droid <rometrics.app@gmail.com>
2026-01-22 01:08:54 -0500

    update

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,18 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const cors = require("cors")({ origin: true });
 
 admin.initializeApp();
 
-exports.createCustomToken = functions.https.onRequest(async (req, res) => {
-  try {
-    const { idToken } = req.body;
-    const decoded = await admin.auth().verifyIdToken(idToken);
-    const customToken = await admin.auth().createCustomToken(decoded.uid);
-    res.json({ customToken });
-  } catch {
-    res.status(401).json({ error: "Invalid token" });
-  }
+exports.createCustomToken = functions.https.onRequest((req, res) => {
+  cors(req, res, async () => {
+    try {
+      const { idToken } = req.body;
+      const decoded = await admin.auth().verifyIdToken(idToken);
+      const customToken = await admin.auth().createCustomToken(decoded.uid);
+      res.json({ customToken });
+    } catch (e) {
+      res.status(401).json({ error: "Invalid token" });
+    }
+  });
 });
